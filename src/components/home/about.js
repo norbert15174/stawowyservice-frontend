4055769f323b6fadd818ebf86e1aeca96ca60b05
faronnor <faronnorbertkrk@gmail.com>
2023-10-31 13:23:26 +0100

    changed instagram position

--- src/components/home/about.js
+++ src/components/home/about.js
@@ -9,7 +9,7 @@ const About = () => (
     }}>
 
         <div className="grid grid-cols-1 gap-3 place-items-center h-screen">
-            <div className="w-10/12 sm:w-10/12 md:w-10/12 lg:w-8/12 sm:h-5/6 h-3/4 lg:h-4/6  flex justify-center bg-black bg-opacity-80 py-10 px-5 xl:p-20 font-normal text-white mb-3 stick-font text-base sm:text-xl md:text-2xl 2xl:text-3xl">
+            <div className="w-11/12 sm:w-10/12 md:w-10/12 lg:w-8/12 sm:h-5/6 h-3/4 lg:h-4/6  flex justify-center bg-black bg-opacity-80 py-10 px-5 xl:p-20 font-normal text-white mb-3 stick-font text-base sm:text-xl md:text-xl 2xl:text-2xl 3xl:text-3xl">
                 <div className="inline-block text-center">
                     <IoMdQuote className=" mx-auto p-1 lg:p-3 w-12 h-12 lg:w-16 lg:h-16 mb-2 mt-2 md:mb-10 md:mt-10 lg:mb-15 lg:mt-15 xl:mb-15 xl:mt-15" />
                     ,,Szukasz unikatowych mebli? Dobrze trafiłeś! Wykonujemy indywidualne zamówienia na stoły, krzesła oraz ławy.
